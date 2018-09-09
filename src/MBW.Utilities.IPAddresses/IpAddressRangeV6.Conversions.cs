f16e32ffc4589c7887a548cc6af06d8ef41c59d4
Michael Bisbjerg <michael@mbwarez.dk>
2018-09-09 18:02:37 +0200

    Use better IPv6 parser

--- src/MBW.Utilities.IPAddresses/IpAddressRangeV6.Conversions.cs
+++ src/MBW.Utilities.IPAddresses/IpAddressRangeV6.Conversions.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Net;
-using System.Net.Sockets;
-using System.Text.RegularExpressions;
 
 namespace MBW.Utilities.IPAddresses
 {
@@ -23,34 +21,157 @@ namespace MBW.Utilities.IPAddresses
                 return false;
             }
 
-            // TODO: High perf parser
-            Match match = IpRegex.Match(value);
-            if (!match.Success)
+            ReadOnlySpan<char> @string = value.AsSpan();
+
+            ulong high = 0;
+            ulong low = 0;
+            byte mask = 0;
+            byte idx = 1;
+
+            void SetTuplet(byte index, ulong newValue)
             {
-                result = default;
-                return false;
+                if (index < 5)
+                    high |= newValue << ((3 - index) * 16);
+                else
+                    low |= newValue << ((7 - index) * 16);
             }
 
-            byte mask = 128;
-
-            if (!IPAddress.TryParse(match.Groups[1].Value, out IPAddress ip) || ip.AddressFamily != AddressFamily.InterNetworkV6)
+            byte ParseChar(char ch)
             {
-                result = default;
-                return false;
+                if ('0' <= ch && ch <= '9')
+                    return (byte)(ch - '0');
+
+                if ('A' <= ch && ch <= 'F')
+                    return (byte)(10 + ch - 'A');
+
+                if ('a' <= ch && ch <= 'f')
+                    return (byte)(10 + ch - 'a');
+
+                return byte.MaxValue;
             }
 
-            Group maskMatch = match.Groups[2];
-            if (maskMatch.Success)
+            // Find mask, read reverse
+            for (int i = @string.Length - 1; i >= 0; i--)
             {
-                mask = byte.Parse(maskMatch.Value);
-                if (mask > 128)
+                char ch = @string[i];
+
+                if (ch == ':' || 'A' <= ch && ch <= 'F' || 'a' <= ch && ch <= 'f')
                 {
-                    result = default;
-                    return false;
+                    // No mask provided
+                    mask = 128;
+                    break;
+                }
+
+                if (ch == '/')
+                {
+                    // Mask has been read, cut off the mask
+                    @string = @string.Slice(0, i);
+                    break;
+                }
+
+                if ('0' <= ch && ch <= '9')
+                {
+                    mask += (byte)((ch - '0') * idx);
+                    idx *= 10;
+
+                    continue;
+                }
+
+                throw new ArgumentException();
+            }
+
+            // Tuple offset
+            idx = 0;
+
+            // Read from front, until :: is met
+            bool doReverse = false;
+
+            while (@string.Length > 0)
+            {
+                ushort currentTuplet = 0;
+
+                // Read up to 4 chars
+                int i;
+                for (i = 0; i < 4; i++)
+                {
+                    char ch = @string[i];
+
+                    if (ch == ':')
+                        break;
+
+                    // Read and append char
+                    byte val = ParseChar(ch);
+
+                    if (val == byte.MaxValue)
+                        throw new ArgumentException();
+
+                    currentTuplet <<= 4;
+                    currentTuplet += val;
+                }
+
+                SetTuplet(idx++, currentTuplet);
+                @string = @string.Slice(i);
+
+                // Check if next char is ':'
+                if (@string[0] == ':')
+                {
+                    @string = @string.Slice(1);
+
+                    // Check if next again is ':' too
+                    if (@string[0] == ':')
+                    {
+                        // We need to parse backwards now
+                        doReverse = true;
+                        break;
+                    }
+                }
+                else
+                    throw new ArgumentException();
+            }
+
+            if (doReverse)
+            {
+                idx = 7;
+
+                while (@string.Length > 0)
+                {
+                    ushort currentTuplet = 0;
+
+                    // Read up to 4 chars
+                    // Find ':'
+
+                    int lastIdx = @string.LastIndexOf(':');
+
+                    ReadOnlySpan<char> segment = @string.Slice(lastIdx + 1);
+                    @string = @string.Slice(0, lastIdx);
+
+                    if (segment.Length == 0 || segment.Length > 4)
+                        // We cannot have two "::" segments, cannot have longer than 4 chars
+                        throw new Exception();
+
+                    int i;
+                    for (i = 0; i < segment.Length; i++)
+                    {
+                        char ch = segment[i];
+
+                        if (ch == ':')
+                            break;
+
+                        // Read and append char
+                        byte val = ParseChar(ch);
+
+                        if (val == byte.MaxValue)
+                            throw new ArgumentException();
+
+                        currentTuplet <<= 4;
+                        currentTuplet += val;
+                    }
+
+                    SetTuplet(idx--, currentTuplet);
                 }
             }
 
-            result = new IpAddressRangeV6(ip, mask);
+            result = new IpAddressRangeV6(high, low, mask);
             return true;
         }
 
