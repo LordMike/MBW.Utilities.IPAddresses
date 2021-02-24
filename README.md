666c010989283d3a9315ba60fda7d9700cfbabab
Michael Bisbjerg <michael@mbwarez.dk>
2021-02-24 19:27:12 +0100

    Readme

--- README.md
+++ README.md
@@ -2,10 +2,18 @@
 
 Fast IPv4 and IPv6 primitives and parsers for .NET.
 
+## Nuget
+
+Find this on [Nuget](https://www.nuget.org/packages/MBW.Utilities.IPAddresses)
+
+> MBW.Utilities.IPAddresses
+
 ## Features
 
 * Supports [multiple formats](src/MBW.Utilities.IPAddresses.Tests/FormatTests.cs) of IPv4 and IPv6 writing
+* Conversion to and from .NET's `IPAddress` type
 * Fast tokenized parsing of addresses
 * Support CIDR-style notation of networks
-* Supports calculations based on networks such as contains and intersection
+* Supports [calculations based on networks](src/MBW.Utilities.IPAddresses.Tests/UtilityTests.cs) such as contains
+* Utilities to make smallest common networks for a set of IP addresses (supernets)
 
---
+++ src/MBW.Utilities.IPAddresses.Tests/UtilityTests.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+
+namespace MBW.Utilities.IPAddresses.Tests
+{
+    public class UtilityTests
+    {
+        [Fact]
+        public void IPv4Utilities()
+        {
+            IpAddressRangeV4 network = IpAddressRangeV4.Parse("192.168.1.0/24");
+            IpAddressRangeV4 ip = IpAddressRangeV4.Parse("192.168.1.1");
+
+            Assert.True(network.Contains(ip));
+            Assert.True(network.ContainsOrEqual(ip));
+            Assert.True(ip.IsContainedIn("192.168.1.0/24"));
+            Assert.True(ip.IsContainedIn("192.168.0.0/16"));
+            Assert.False(ip.IsContainedIn("192.168.1.128/25"));
+
+            Assert.Equal("192.168.1.1", ip.ToString());
+            Assert.Equal("192.168.1.1/32", ip.ToString(true));
+            Assert.Equal("192.168.1.0/24", network.ToString());
+            Assert.Equal("192.168.1/24", network.ToPrefixString());
+        }
+    }
+}
