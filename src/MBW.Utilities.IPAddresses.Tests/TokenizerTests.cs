58517b797b12701e8cd6cb6d1cd28d282dd0a9e5
Michael Bisbjerg <michael@mbwarez.dk>
2022-04-06 18:45:37 +0200

    Improve token tests

--- src/MBW.Utilities.IPAddresses.Tests/TokenizerTests.cs
+++ src/MBW.Utilities.IPAddresses.Tests/TokenizerTests.cs
@@ -1,7 +1,7 @@
 ﻿using FluentAssertions;
 using MBW.Utilities.IPAddresses.Tokenization;
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using Xunit;
 
 namespace MBW.Utilities.IPAddresses.Tests;
@@ -11,87 +11,34 @@ public class TokenizerTests
     [Fact]
     public void EmptyTest()
     {
-        Tokenizer tokenizer = new Tokenizer(string.Empty);
+        void Test(Func<ParsedToken> func) => func().Type.Should().Be(TokenType.None);
 
-        ParsedToken parsed = tokenizer.ParseAndAdvanceStart();
-        parsed.Type.Should().Be(TokenType.None);
-
-        parsed = tokenizer.ParseAndAdvanceEnd();
-        parsed.Type.Should().Be(TokenType.None);
-
-        parsed = tokenizer.PeekStart();
-        parsed.Type.Should().Be(TokenType.None);
-
-        parsed = tokenizer.PeekEnd();
-        parsed.Type.Should().Be(TokenType.None);
+        Test(() => new Tokenizer(string.Empty).ParseAndAdvanceStart());
+        Test(() => new Tokenizer(string.Empty).ParseAndAdvanceEnd());
+        Test(() => new Tokenizer(string.Empty).PeekStart());
+        Test(() => new Tokenizer(string.Empty).PeekEnd());
     }
 
     [Fact]
     public void ReverseTest()
     {
-        Tokenizer tokenizer = new Tokenizer("192.168");
+        Tokenizer tokenizer = new Tokenizer("192.168.55/44");
 
-        ParsedToken parsed = tokenizer.PeekEnd(false);
-        parsed.Type.Should().Be(TokenType.Number);
-        parsed.Value.Should().Be(168);
+        ParsedToken[] expected = new ParsedToken[]
+        {
+            new(TokenType.Number, 44),
+            new(TokenType.Slash, 0),
+            new(TokenType.Number, 55),
+            new(TokenType.Dot, 0),
+            new(TokenType.Number, 168),
+            new(TokenType.Dot, 0),
+            new(TokenType.Number, 192)
+        };
 
-        parsed = tokenizer.ParseAndAdvanceEnd(false);
-        parsed.Type.Should().Be(TokenType.Number);
-        parsed.Value.Should().Be(168);
+        IEnumerable<ParsedToken> tokens = tokenizer.ParseAllEnd(false);
+        tokens.Should().Equal(expected);
 
-        parsed = tokenizer.PeekEnd(false);
-        parsed.Type.Should().Be(TokenType.Dot);
-
-        parsed = tokenizer.ParseAndAdvanceEnd(false);
-        parsed.Type.Should().Be(TokenType.Dot);
-
-        parsed = tokenizer.PeekEnd(false);
-        parsed.Type.Should().Be(TokenType.Number);
-        parsed.Value.Should().Be(192);
-
-        parsed = tokenizer.ParseAndAdvanceEnd(false);
-        parsed.Type.Should().Be(TokenType.Number);
-        parsed.Value.Should().Be(192);
-
-        parsed = tokenizer.PeekEnd(false);
-        parsed.Type.Should().Be(TokenType.None);
-
-        parsed = tokenizer.ParseAndAdvanceEnd(false);
-        parsed.Type.Should().Be(TokenType.None);
-    }
-
-    [Fact]
-    public void GenericNumberTest()
-    {
-        Tokenizer tokenizer = new Tokenizer("192.168");
-
-        ParsedToken parsed = tokenizer.PeekStart(false);
-        parsed.Type.Should().Be(TokenType.Number);
-        parsed.Value.Should().Be(192);
-
-        parsed = tokenizer.ParseAndAdvanceStart(false);
-        parsed.Type.Should().Be(TokenType.Number);
-        parsed.Value.Should().Be(192);
-
-        parsed = tokenizer.PeekStart(false);
-        parsed.Type.Should().Be(TokenType.Dot);
-
-        parsed = tokenizer.ParseAndAdvanceStart(false);
-        parsed.Type.Should().Be(TokenType.Dot);
-
-        parsed = tokenizer.PeekStart(false);
-        parsed.Type.Should().Be(TokenType.Number);
-        parsed.Value.Should().Be(168);
-
-        tokenizer.ParseAndAdvanceStart(false);
-        parsed.Type.Should().Be(TokenType.Number);
-        parsed.Value.Should().Be(168);
-
-        parsed =  tokenizer.PeekStart(false);
-        parsed.Type.Should().Be(TokenType.None);
-
-        parsed = tokenizer.ParseAndAdvanceStart(false);
-        parsed.Type.Should().Be(TokenType.None);
+        tokenizer.ParseAndAdvanceStart().Type.Should().Be(TokenType.None);
     }
 
     [Fact]
@@ -99,16 +46,50 @@ public class TokenizerTests
     {
         Tokenizer tokenizer = new("192.168.114.255/32");
 
-        List<ParsedToken> parsed = tokenizer.ParseAllStart(false).ToList();
-        Assert.Equal(9, parsed.Count);
+        ParsedToken[] expected = new ParsedToken[]
+        {
+            new(TokenType.Number, 192),
+            new(TokenType.Dot, 0),
+            new(TokenType.Number, 168),
+            new(TokenType.Dot, 0),
+            new(TokenType.Number, 114),
+            new(TokenType.Dot, 0),
+            new(TokenType.Number, 255),
+            new(TokenType.Slash, 0),
+            new(TokenType.Number, 32)
+        };
+
+        IEnumerable<ParsedToken> tokens = tokenizer.ParseAllStart(false);
+        tokens.Should().Equal(expected);
+
+        tokenizer.ParseAndAdvanceStart().Type.Should().Be(TokenType.None);
     }
 
     [Fact]
     public void ComplexValueIPv6()
     {
-        Tokenizer tokenizer = new("2001:0dff:44ff:0:1744:ffff/64");
+        Tokenizer tokenizer = new("2001:0dff:44ff::0:1744:ffff/64");
 
-        List<ParsedToken> parsed = tokenizer.ParseAllStart().ToList();
-        Assert.Equal(13, parsed.Count);
+        ParsedToken[] expected = new ParsedToken[]
+        {
+            new(TokenType.Number, 0x2001),
+            new(TokenType.Colon, 0),
+            new(TokenType.Number, 0x0dff),
+            new(TokenType.Colon, 0),
+            new(TokenType.Number, 0x44ff),
+            new(TokenType.DoubleColon, 0),
+            new(TokenType.Number, 0),
+            new(TokenType.Colon, 0),
+            new(TokenType.Number, 0x1744),
+            new(TokenType.Colon, 0),
+            new(TokenType.Number, 0xffff),
+            new(TokenType.Slash, 0),
+            new(TokenType.Number, 0x64) // This would normally be base-10, but in this test it will be come hex.
+        };
+
+        IEnumerable<ParsedToken> tokens = tokenizer.ParseAllStart();
+        tokens.Should().Equal(expected);
+
+        tokenizer.ParseAndAdvanceStart().Type.Should().Be(TokenType.None);
     }
 }
