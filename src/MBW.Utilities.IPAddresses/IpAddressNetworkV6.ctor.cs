13c3ea713f533f26c24c0ae77a9234a24e32b63d
Michael Bisbjerg <michael@mbwarez.dk>
2022-04-18 16:04:26 +0200

    Simplify IpNetworkV6 ctors

--- src/MBW.Utilities.IPAddresses/IpAddressNetworkV6.ctor.cs
+++ src/MBW.Utilities.IPAddresses/IpAddressNetworkV6.ctor.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net;
 using System.Net.Sockets;
+using Dirichlet.Numerics;
 using MBW.Utilities.IPAddresses.Helpers;
 
 namespace MBW.Utilities.IPAddresses;
@@ -27,50 +28,39 @@ public partial struct IpAddressNetworkV6
     }
 
     public IpAddressNetworkV6(IPAddress address, byte mask)
-        : this(address.GetAddressBytes(), mask)
     {
         if (address.AddressFamily != AddressFamily.InterNetworkV6)
             throw new ArgumentException();
+
+        byte[] bytes = address.GetAddressBytes();
+        ulong high = BitUtilities.Reverse(BitConverter.ToUInt64(bytes, 0));
+        ulong low = BitUtilities.Reverse(BitConverter.ToUInt64(bytes, 8));
+
+        _networkAddress = new IpAddressV6(high, low);
+        _mask = mask;
+
+        TruncateNetworkAddress(ref _networkAddress);
     }
 
     public IpAddressNetworkV6(IpAddressV6 address, byte mask)
-        : this(address.AddressHigh, address.AddressLow, mask)
     {
-    }
+        _networkAddress = address;
+        _mask = mask;
 
-    private IpAddressNetworkV6(byte[] address, byte mask)
-        : this(BitUtilities.Reverse(BitConverter.ToUInt64(address, 0)), BitUtilities.Reverse(BitConverter.ToUInt64(address, 8)), mask)
-    {
-        if (address.Length != 16)
-            throw new ArgumentException();
+        TruncateNetworkAddress(ref _networkAddress);
     }
 
     public IpAddressNetworkV6(ulong addressHigh, ulong addressLow, byte mask)
     {
-        _mask = mask;
-        if (mask == 0)
-        {
-            addressHigh = addressLow = 0;
-        }
-        else if (mask == 128)
-        {
-        }
-        else if (mask > 64)
-        {
-            // Keep the high bits, truncate the lows
-            addressLow &= ulong.MaxValue << (64 - (_mask - 64));
-        }
-        else if (mask == 64)
-        {
-            addressLow = 0;
-        }
-        else
-        {
-            // Truncate the high bits, remove the lows
-            addressHigh &= ulong.MaxValue << (64 - _mask);
-            addressLow = 0;
-        }
-
         _networkAddress = new IpAddressV6(addressHigh, addressLow);
+        _mask = mask;
+
+        TruncateNetworkAddress(ref _networkAddress);
+    }
+
+    private void TruncateNetworkAddress(ref IpAddressV6 address)
+    {
+        IpAddressV6 networkMask = _networkMasks[_mask];
+        address &= networkMask;
     }
 }
